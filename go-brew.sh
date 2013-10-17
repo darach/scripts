5463aee4a3c5a5036af19b722ca0559f4868b566
Darach Ennis <darach@gmail.com>
2013-10-17 12:28:07 +0100

    Add wrk http benchmarking performance tool

--- go-brew.sh
+++ go-brew.sh
@@ -2,3 +2,15 @@
 
 # Get a brew on
 ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"
+
+# Install a few essentials
+brew install htop
+brew install cairo
+brew install hwloc
+
+# HTTP perf testing - ab OOTB
+brew install nginx
+brew install siege
+brew install httperf
+brew install wrk
+
